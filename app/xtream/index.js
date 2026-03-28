dbb99933f7dec8aa14d7d7fc29323cb756e1dd3f
drpurna <94000424+drpurna@users.noreply.github.com>
2026-03-28 17:55:02 +0530

    Delete app/xtream/index.js

--- app/xtream/index.js
+++
@@ -1,9 +0,0 @@
-// xtream/index.js
-// Main export for Xtream Codes integration
-
-import XtreamClient from './client.js';
-import XtreamCache from './cache.js';
-import XtreamAuth from './auth.js';
-
-export { XtreamClient, XtreamCache, XtreamAuth };
-export default XtreamClient;
